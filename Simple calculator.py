dbba010a7f0c8b64c6a7b90742c838db5b78cd35
MaheshwariRoy <114797622+MaheshwariRoy@users.noreply.github.com>
2022-10-08 11:24:28 +0530

    Add files via upload

---
+++ Simple calculator.py
@@ -0,0 +1,15 @@
+print("Simple Calculator")
+x = 40
+y = 50
+
+sum = x+y
+difference = x-y
+product = x*y
+quotient = x/y
+remainder = x % y
+
+print("The sum pf x and y is ", sum)
+print("The difference of x and y is ", difference)
+print("The product of x and y is ", product)
+print("The quotient of x and y is ", quotient)
+print("The remainder of x and y is ", remainder)
